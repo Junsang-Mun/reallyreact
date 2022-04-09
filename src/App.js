c1b36c2f9bf9f6dccd63b5cda7677b8152342529
Junsang-mun <mun.js@aol.com>
2022-04-09 11:10:24 +0900

    learning react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("I run every time");
+  const runOnlyOnce = () => console.log("I run only once!");
+  useEffect(runOnlyOnce, []);
   return (
     <div>
-      <h1>Welcome Back!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Button</button>
     </div>
   );
 }
