647e92e7df23b1c96fc0847be204496125d43d4e
Junsang-mun <mun.js@aol.com>
2022-04-09 19:16:17 +0900

    cryptocurrency project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [currency, setCurrency] = useState("");
+  const [reload, setReload] = useState(0);
+  const onClick = () => {
+    setLoading(true);
+    setReload((prev) => prev + 1);
   };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+    fetch("https://api.exchangerate.host/latest?base=USD&symbols=KRW")
+      .then((response) => response.json())
+      .then((json) => {
+        let parsed = json.rates.KRW;
+        setCurrency(parsed);
+      });
+  }, [reload]);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>Coin charts</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <button onClick={onClick}>Click to Reload</button>
+          <ul>
+            {coins.map((coin) => (
+              <li>
+                <strong>{coin.name}</strong> ({coin.symbol}) :{" "}
+                <strong> $ {coin.quotes.USD.price.toFixed(4)}</strong> | ₩{" "}
+                {(coin.quotes.USD.price.toFixed(2) * currency)
+                  .toFixed(2)
+                  .toString()
+                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
