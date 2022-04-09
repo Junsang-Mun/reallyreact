22a6f3df75ade35c089c2273b738853144bfa88f
Junsang-mun <mun.js@aol.com>
2022-04-09 23:47:33 +0900

    movie list updated

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import { useEffect, useState } from "react";
 function App() {
   const [isLoading, setLoadingState] = useState(true);
   const [movieList, setMovieList] = useState([]);
-  useEffect(() => {
-    fetch(
+  const getMovieList = async () => {
+    const response = await fetch(
       "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => setMovieList(json.data.movies))
-      .then(setLoadingState(false));
-  }, []);
+    );
+    const json = await response.json();
+    setMovieList(json.data.movies);
+    setLoadingState(false);
+  };
   console.log(movieList);
+  useEffect(() => {
+    getMovieList();
+  }, []);
   return (
     <div>
-      <h1>Mooflix</h1>
       {isLoading ? (
         <strong>Loading...</strong>
       ) : (
         <div>
-          <p>asdfasdf</p>
+          {movieList.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+            </div>
+          ))}
         </div>
       )}
     </div>
