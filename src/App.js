bebcf5b7d41806777dfa7655737c707d62681158
Junsang-mun <mun.js@aol.com>
2022-04-09 12:05:33 +0900

    part #6 complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
-  console.log("I run every time");
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("run");
   const runOnlyOnce = () => console.log("I run only once!");
   useEffect(runOnlyOnce, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 3) {
+      console.log("search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Button</button>
     </div>
