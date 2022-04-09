7ae090cf752523f45ef80d3da28a0a58fadf705c
Junsang-mun <mun.js@aol.com>
2022-04-09 23:28:54 +0900

    working on Movie app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [currency, setCurrency] = useState("");
-  const [reload, setReload] = useState(0);
-  const onClick = () => {
-    setLoading(true);
-    setReload((prev) => prev + 1);
-  };
+  const [isLoading, setLoadingState] = useState(true);
+  const [movieList, setMovieList] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    )
       .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-    fetch("https://api.exchangerate.host/latest?base=USD&symbols=KRW")
-      .then((response) => response.json())
-      .then((json) => {
-        let parsed = json.rates.KRW;
-        setCurrency(parsed);
-      });
-  }, [reload]);
+      .then((json) => setMovieList(json.data.movies))
+      .then(setLoadingState(false));
+  }, []);
+  console.log(movieList);
   return (
     <div>
-      <h1>Coin charts</h1>
-      {loading ? (
+      <h1>Mooflix</h1>
+      {isLoading ? (
         <strong>Loading...</strong>
       ) : (
         <div>
-          <button onClick={onClick}>Click to Reload</button>
-          <ul>
-            {coins.map((coin) => (
-              <li>
-                <strong>{coin.name}</strong> ({coin.symbol}) :{" "}
-                <strong> $ {coin.quotes.USD.price.toFixed(4)}</strong> | ₩{" "}
-                {(coin.quotes.USD.price.toFixed(2) * currency)
-                  .toFixed(2)
-                  .toString()
-                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
-              </li>
-            ))}
-          </ul>
+          <p>asdfasdf</p>
         </div>
       )}
     </div>
